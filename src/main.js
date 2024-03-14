a6d568cafd82f29dd1087d361126f0d9c57fe7b1
daw2-barrio23 <113451779+daw2-barrio23@users.noreply.github.com>
2024-03-14 19:17:41 +0000

    gh-pages funcionando #13

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-import { header } from './componentes/header'
+// Import all of Bootstrap's JS
+import 'bootstrap'
+
+// Import our custom CSS
+import './scss/style.scss'
+
+
 import { footer } from './componentes/footer'
+import { header } from './componentes/header'
+
 
 // Importamos la vista por defecto (que será home)
 async function cargarVista () {
-  const componente = await import('./vistas/homeVista')
+  const componente = await import('./vistas/home')
   const vista = componente.default
   // Inyectamos la vista home
   document.querySelector('main').innerHTML = vista.template
