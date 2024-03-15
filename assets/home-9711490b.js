e94db5d78d15d9299aed77a21342b60a9afdf848
daw2-barrio23 <113451779+daw2-barrio23@users.noreply.github.com>
2024-03-15 14:55:45 +0000

    Updates

---
+++ assets/home-9711490b.js
@@ -0,0 +1,18 @@
+const home = {
+  template: (
+    // html
+    `
+  <div class="container">
+    <h1 class="mt-5 text-center fw-bold" style="font-size: 100px">
+      PaginaF1
+    </h1>
+    <div class="m-5 mx-auto" style="max-width: 400px">
+      <img src="images/F1.png" alt="fpllefia" class="img-fluid" />
+    </div>
+  </div>
+    `
+  )
+};
+export {
+  home as default
+};
