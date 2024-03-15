97ab12d64ac42de41332ca291715c1ef9f52e80a
daw2-barrio23 <113451779+daw2-barrio23@users.noreply.github.com>
2024-03-15 14:54:56 +0000

    vistas y componentes #14

--- src/componentes/footer.js
+++ src/componentes/footer.js
@@ -1,3 +1,22 @@
 export const footer = {
-    template: `footer`
-}
+    template: // html
+    `
+  <nav class="navbar bg-secondary fixed-bottom small">
+    <div class="container">
+      <a class="navbar-brand" href="http://www.fpllefia.com">
+        <img
+          src="images/F1.png"
+          alt="fpllefia"
+          width="30"
+          height="24"
+          class="d-inline-block align-text-top"
+        />
+        FPLlefià
+      </a>
+      <span class="navbar-text">@Texto de header</span>
+      <a href="#" class="nav-link">Vínculo header</a>
+    </div>
+  </nav>
+    `
+  }
+  
--- src/vistas/home.js
+++ src/vistas/home.js
@@ -1,3 +1,14 @@
 export default {
-    template: `<h1>home</h1>`
-}
+    template: // html
+    `
+  <div class="container">
+    <h1 class="mt-5 text-center fw-bold" style="font-size: 100px">
+      PaginaF1
+    </h1>
+    <div class="m-5 mx-auto" style="max-width: 400px">
+      <img src="images/F1.png" alt="fpllefia" class="img-fluid" />
+    </div>
+  </div>
+    `
+  }
+  
